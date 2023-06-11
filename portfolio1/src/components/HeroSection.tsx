2e437d7e3a7e2058a09cbe5a0151a663a0615741
Dedios <dediosprogramming@gmail.com>
2023-06-11 00:04:25 -0400

    added copy to Hero

--- portfolio1/src/components/HeroSection.tsx
+++ portfolio1/src/components/HeroSection.tsx
@@ -6,6 +6,12 @@ export default function HeroSection() {
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Eddie P</h1>
             <p>Full Stack Engineer</p>
+            <p>
+              Highly skilled and professional JavaScript/Java Software
+              Engineer/Web Developer. I am passionate about solving problems
+              through Technology and building cool things 😛 for the Human Race.
+            </p>
+            {/* put a button that takes them to the about you section */}
           </div>
         </div>
       </div>
