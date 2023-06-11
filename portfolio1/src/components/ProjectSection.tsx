1ce4cb66db1ada048d8aeaa1e64f0addc495d966
Dedios <dediosprogramming@gmail.com>
2023-06-10 23:53:29 -0400

    implementing Card

--- portfolio1/src/components/ProjectSection.tsx
+++ portfolio1/src/components/ProjectSection.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import logo from "@/images/logo.png";
+import Card from "@/components/Card";
 
 export default function ProjectSection() {
-  return <div className="min-h-full h-80"></div>;
+  return (
+    <div className="min-h-full h-80">
+      <Card />
+    </div>
+  );
 }
