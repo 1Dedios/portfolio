81e6a5a0c7da09579a03fb58e5096f8934d932a4
Dedios <dediosprogramming@gmail.com>
2023-06-10 20:43:43 -0400

    refactored bg color

--- portfolio1/src/components/Footer.tsx
+++ portfolio1/src/components/Footer.tsx
@@ -7,7 +7,7 @@ import { UilEnvelopeMinus } from "@iconscout/react-unicons";
 export default function Footer() {
   return (
     <>
-      <div className="footer footer-center p-10 bg-purple text-primary-content">
+      <div className="footer footer-center p-10 bg-neutral-50/50 text-neutral-100">
         <div>
           <Image src={logo} alt="Watermelon Me" width={250} height={100} />
 
