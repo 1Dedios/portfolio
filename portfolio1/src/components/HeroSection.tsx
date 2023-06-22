c542ba5b30d98ad137807fac1bb763f101604e33
Dedios <dediosprogramming@gmail.com>
2023-06-22 15:42:30 -0400

    added an image to the bg

--- portfolio1/src/components/HeroSection.tsx
+++ portfolio1/src/components/HeroSection.tsx
@@ -1,31 +1,29 @@
 import Button from "@/components/Button";
-import background from "/public/dedios_logo.png";
+import Image from "next/image";
+import watermelon from "/public/watermelon_me_bitmoji.png";
 
 export default function HeroSection() {
   let buttonText = "...learn more";
 
   return (
-    <div style={{background-image: url(/public/dedios_logo.png)}}>
-      <div className="hero min-h-screen">
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url(/public/dedios_logo.png)",
+        }}
+      >
         <div className="hero-content text-center">
           <div className="max-w-md">
+            <Image src={watermelon} alt="watermelon logo" />
             <h1 className="text-5xl font-bold">Eddie P</h1>
-            <h3>Full Stack Engineer</h3>
-            <p>
-<<<<<<< HEAD
+            <h2>Full Stack Engineer</h2>
+            <p className="pt-5">
               Highly skilled, professional, polyglot Software Developer. I am
-=======
-              Highly skilled, professional polyglot Software Developer. I am
->>>>>>> 6ba8218bda07940fd7d13173f21ccd4805c408d3
               passionate about solving problems through Technology and building
               cool things 😛 for the Human Race.
             </p>
-            <Button
-              className={
-                "bg-primary hover:bg-cyan-600 border-yellow-400 p-2 border-double"
-              }
-              text={buttonText}
-            />
+            <Button className={"bg-primary"} text={buttonText} />
           </div>
         </div>
       </div>
