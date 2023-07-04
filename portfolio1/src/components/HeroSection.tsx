f00f8c4f3679651368848546f1ac8e132e12e571
Dedios <dediosprogramming@gmail.com>
2023-07-04 17:44:25 -0400

    styled button

--- portfolio1/src/components/HeroSection.tsx
+++ portfolio1/src/components/HeroSection.tsx
@@ -3,11 +3,10 @@ import Image from "next/image";
 import watermelon from "/public/watermelon_me_bitmoji.png";
 
 export default function HeroSection() {
-  let buttonText = "...learn more";
+  let buttonText = "... learn more";
 
   return (
     <div>
-      <h1>testing</h1>
       <div
         className="hero min-h-screen bg-auto bg-purple"
         style={{
@@ -23,8 +22,13 @@ export default function HeroSection() {
               cool things 😛 for the Human Race.
             </p> */}
             <h1 className="text-5xl font-bold">Eddie P</h1>
-            <h2 className="font-bold italic">Full-Stack Engineer</h2>
-            <Button className={"bg-primary"} text={buttonText} />
+            <h2 className="font-bold italic mt-4">Full-Stack Engineer</h2>
+            <Button
+              className={
+                "bg-purple p-2 rounded-md border-2 border-rose-500 mt-2"
+              }
+              text={buttonText}
+            />
           </div>
           <span className="chat chat-start text-center w-64 h-40 -mx-20">
             <div className="chat-bubble chat-bubble-warning shadow-md shadow-slate-200">
