85e28be23e73f4ee0ca0d05f2edc07ae1216abda
Dedios <dediosprogramming@gmail.com>
2023-07-09 16:08:29 -0400

    added Benefits component

---
+++ portfolio1/src/components/Benefits.tsx
@@ -0,0 +1,25 @@
+export default function Benefits() {
+  return (
+    <div className="bg-white h-64">
+      <h1 className="text-black text-center pt-4">Benefits of My Work</h1>
+      <div className="divider"></div>
+      <div className="flex flex-row space-x-4">
+        <div>
+          {/* icon */}
+          <h3 className="text-black">Acute Attention to Detail</h3>
+          {/* content */}
+        </div>
+        <div>
+          {/* icon */}
+          <h3 className="text-black">Modern Design</h3>
+          {/* content */}
+        </div>
+        <div>
+          {/* icon */}
+          <h3 className="text-black">P.P.P</h3>
+          <p className="text-black">pixel perfect perfection</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- portfolio1/src/pages/index.tsx
+++ portfolio1/src/pages/index.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import Footer from "@/components/Footer";
 import ProjectSection from "@/components/ProjectSection";
+import Benefits from "@/components/Benefits";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function Home() {
         <Navbar />
         <HeroSection />
         <ProjectSection />
+        <Benefits />
         <Footer />
       </div>
     </main>
