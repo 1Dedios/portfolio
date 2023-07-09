39a026bff047ae4c5db068b9b0409d4b901136a0
Dedios <dediosprogramming@gmail.com>
2023-07-09 14:52:36 -0400

    spacing fixed

--- portfolio1/src/components/ProjectSection.tsx
+++ portfolio1/src/components/ProjectSection.tsx
@@ -1,18 +1,21 @@
 import Card from "@/components/Card";
 import logo from "/public/logo.png";
+import Button from "@/components/Button";
 
 export default function ProjectSection() {
   let firstCardAltText = "random";
   let firstCardText = "checking in";
   let firstCardTitle = "title";
   let firstButtonText = "text";
+  let buttonText = "👀 More";
 
   return (
-    <div className="min-h-full h-fit px-4 py-8">
+    <div className="min-h-full h-full px-20 py-8">
       <h1 className="text-6xl text-center font-subs font-extrabold italic pb-10">
         Projects
       </h1>
-      <div className="flex flex-row space-x-4">
+      <Button text={buttonText} className={"align-middle"} />
+      <div className="flex flex-row space-x-4 justify-evenly">
         <Card
           image={logo}
           alt={firstCardAltText}
