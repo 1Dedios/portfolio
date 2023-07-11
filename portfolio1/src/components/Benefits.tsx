e0187308ec0bd26d21a439fbb9e05fd107e79d5b
Dedios <dediosprogramming@gmail.com>
2023-07-11 19:23:29 -0400

    refactored Benefits

--- portfolio1/src/components/Benefits.tsx
+++ portfolio1/src/components/Benefits.tsx
@@ -1,23 +1,42 @@
 export default function Benefits() {
   return (
     <div className="bg-white h-64">
-      <h1 className="text-black text-center pt-4">Benefits of My Work</h1>
+      <h1 className="text-black text-center pt-4">
+        Benefits of Working with me
+      </h1>
       <div className="divider"></div>
       <div className="flex flex-row space-x-4">
         <div>
           {/* icon */}
-          <h3 className="text-black">Acute Attention to Detail</h3>
-          {/* content */}
+          <h3 className="text-black">Technically Proficient</h3>
+          <p className="text-black">
+            as a self taught dev I have not ignored the importance of a strong
+            foundation. thus I've immersed myself in Computer Science
+            fundamentals, concepts and processes. this has helped me become a
+            more knowledgable Software Engineer because I understand the
+            underlying structure for running software and why optimizing that
+            foundation only makes for better software.
+          </p>
         </div>
         <div>
           {/* icon */}
-          <h3 className="text-black">Modern Design</h3>
-          {/* content */}
+          <h3 className="text-black">Problem Solving Skills</h3>
+          <p className="text-black">
+            Problem? what is that? I only focus on solution or finding them. As
+            a software engineer there is either problems that have been solved
+            or problems yet to solve. no inbetween. So I seek solutions, whether
+            that's from my network, books, online resources, or tech communities
+            on Discord - I am part of several - but most importantly, I have a
+            mentor in my ring corner who has been doing this for 25+ years - so
+            the wealth of knowledge at my disposal is not quantifiable.{" "}
+          </p>
         </div>
         <div>
           {/* icon */}
-          <h3 className="text-black">P.P.P</h3>
-          <p className="text-black">pixel perfect perfection</p>
+          <h3 className="text-black">
+            effective collaborator and communicator
+          </h3>
+          <p className="text-black">collaboration and </p>
         </div>
       </div>
     </div>
