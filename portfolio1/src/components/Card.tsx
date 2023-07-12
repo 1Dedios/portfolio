bf89ecdfdae9f302f70d04b48362197b249a5281
Dedios <dediosprogramming@gmail.com>
2023-07-12 01:29:19 -0400

    implementing Card component's flip

--- portfolio1/src/components/Card.tsx
+++ portfolio1/src/components/Card.tsx
@@ -4,20 +4,29 @@ interface text {
   cardTitle: string;
   cardText: string;
   alt: string;
+  image: any;
 }
 
 export default function Card({ image }, { alt, cardText, cardTitle }: text) {
   return (
-    <div className="card w-96">
-      <figure>
-        <Image src={image} alt={alt} />
-      </figure>
-      <div className="card-front">
-        <h2>{cardTitle}</h2>
-      </div>
-      <div className="card-back">
-        <div className="card-body">
-          <p>{cardText}</p>
+    <div className="card_flipper w-96">
+      <div className="card_inner">
+        <div className="card_front">
+          <h2>{cardTitle}</h2>
+        </div>
+        <div className="card_back">
+          <div className="card_content">
+            <div className="card_header">
+              <figure>
+                <Image src={image} alt={alt} />
+              </figure>
+              <h2>title</h2>
+            </div>
+            <div className="card_body">
+              <h3>JS Wizard</h3>
+              <p>{cardText}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
