c1ee8f029c52b20579591b95a40d631e441c2824
Dedios <dediosprogramming@gmail.com>
2024-05-13 23:03:31 -0400

    refactoring

--- portfolio1/src/components/Benefits.tsx
+++ portfolio1/src/components/Benefits.tsx
@@ -1,20 +1,12 @@
-import logo from '../../public/logo.png';
-
 export default function Benefits() {
-  let cardTitle: string = 'Technically Proficient';
-  let altText: string = 'yerr';
-  let cardText: string =
-    "as a self taught dev I have not ignored the importance of a strong foundation. thus I've immersed myself in Computer Science fundamentals, concepts and processes. this has helped me become a more knowledgable Software Engineer because I understand the underlying structure for running software and why optimizing that foundation only makes for better software";
-  let buttonText: string = 'testing';
-
   return (
     <div className="bg-white h-80">
-      <h1 className="text-black text-center pt-4">Benefits of My Work</h1>
+      <h1 className="text-black text-center pt-4">BENEFITS OF MY WORK</h1>
       <div className="divider"></div>
       <div className="flex flex-row space-x-4">
         <div>
           {/* icon */}
-          <h3 className="text-black text-center">Problem Solving Skills</h3>
+          <h3 className="text-black text-center">PROBLEM SOLVING SKILLS</h3>
           <p className="text-black text-center">
             Problem? what is that? I only focus on solution or finding them. As
             a software engineer there is either problems that have been solved
@@ -25,10 +17,26 @@ export default function Benefits() {
             the wealth of knowledge at my disposal is not quantifiable.
           </p>
         </div>
+        <div className="divider divider-horizontal">❤️</div>
+        <div>
+          {/* icon */}
+          <h3 className="text-black text-center">PROBLEM SOLVING SKILLS</h3>
+          <p className="text-black text-center">
+            Problem? what is that? I only focus on solution or finding them. As
+            a software engineer there is either problems that have been solved
+            or problems yet to solve. no inbetween. So I seek solutions, whether
+            that`s from my network, books, online resources, or tech communities
+            on Discord - I am part of several - but most importantly, I have a
+            mentor in my ring corner who has been doing this for 25+ years - so
+            the wealth of knowledge at my disposal is not quantifiable.
+          </p>
+        </div>
+        {/* TODO: REPLACE THE HEARTS WITH A COOL ICON */}
+        <div className="divider divider-horizontal">❤️</div>
         <div>
           {/* icon */}
           <h3 className="text-black text-center">
-            effective collaborator and communicator
+            EFFECTIVE COLLABORATOR AND COMMUNICATOR
           </h3>
           <p className="text-black text-center">
             The value of collaboration and communication cannot be understated.
--- portfolio1/src/components/HeroSection.tsx
+++ portfolio1/src/components/HeroSection.tsx
@@ -1,16 +1,16 @@
-import Button from "@/components/Button";
-import Image from "next/image";
-import watermelon from "/public/watermelon_me_bitmoji.png";
+import Button from '@/components/Button';
+import Image from 'next/image';
+import watermelon from '/public/watermelon_me_bitmoji.png';
 
 export default function HeroSection() {
-  let buttonText = "... learn more";
+  let buttonText = '... learn more';
 
   return (
     <div>
       <div
         className="hero min-h-screen bg-auto bg-purple"
         style={{
-          backgroundImage: "url(/dedios_logo.png)",
+          backgroundImage: 'url(/dedios_logo.png)',
         }}
       >
         <div className="hero-content text-center">
@@ -26,12 +26,12 @@ export default function HeroSection() {
             </h2>
             <Button
               className={
-                "bg-purple p-2 rounded-md border-2 border-rose-500 mt-2 font-sans"
+                'bg-purple p-2 rounded-md border-2 border-rose-500 mt-2 font-sans'
               }
               text={buttonText}
             />
           </div>
-          <span className="chat chat-end text-center w-64 h-40 inline-block absolute top-40 bottom-15 left-20 mx-40 my-40">
+          <span className="chat chat-end text-center w-64 h-40 inline-block absolute top-40 bottom-15 left-20 mx-80 my-40">
             <div className="chat-bubble chat-bubble-warning shadow-md shadow-slate-200 font-sans ">
               Highly skilled, professional, polyglot Software Developer. I am
               passionate about solving problems through Technology and building
