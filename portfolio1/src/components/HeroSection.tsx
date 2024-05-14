2e6eb9bc0bf81c5c59cbb61b4f943d812d517f0b
Dedios <dediosprogramming@gmail.com>
2024-05-14 19:19:42 -0400

    refactoring HeroSection

--- portfolio1/src/components/HeroSection.tsx
+++ portfolio1/src/components/HeroSection.tsx
@@ -1,10 +1,7 @@
-import Button from '@/components/Button';
 import Image from 'next/image';
 import watermelon from '/public/watermelon_me_bitmoji.png';
 
 export default function HeroSection() {
-  let buttonText = '... learn more';
-
   return (
     <div>
       <div
@@ -15,26 +12,22 @@ export default function HeroSection() {
       >
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <div className="z-0">
+            <div className="z-0 my-5">
               <Image src={watermelon} alt="watermelon logo" />
             </div>
             <h1
-              className={`font-nosifer text-8xl font-extrabold absolute -my-20 mx-20 -z-10`}
+              className={`font-nosifer text-9xl font-extrabold absolute -my-20 -mx-20 -z-10 text-creme`}
             >
               Eddie P
             </h1>
-            <h2 className={`font-poppins font-extrabold text-2xl italic mt-4`}>
+            <h2
+              className={`font-poppins font-extrabold text-2xl italic mt-20 pt-5 text-creme`}
+            >
               Software Engineer & Web Master
             </h2>
-            <Button
-              className={
-                'bg-purple p-2 rounded-md border-2 border-rose-500 mt-2 font-sans'
-              }
-              text={buttonText}
-            />
           </div>
           <span className="chat chat-end text-center w-64 h-40 inline-block absolute top-40 bottom-15 left-20 mx-80 my-40">
-            <div className="chat-bubble chat-bubble-warning shadow-md shadow-slate-200 font-sans ">
+            <div className="chat-bubble bg-creme shadow-md shadow-slate-200 font-sans text-navy">
               Highly skilled, professional, polyglot Software Developer. I am
               passionate about solving problems through Technology and building
               cool things 😛 for the Human Race.
