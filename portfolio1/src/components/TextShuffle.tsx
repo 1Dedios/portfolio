9c0b97dbcc846db65230346f229d693bef587271
Dedios <dediosprogramming@gmail.com>
2024-06-17 22:48:57 -0400

    TextShuffle component init

---
+++ portfolio1/src/components/TextShuffle.tsx
@@ -0,0 +1,19 @@
+// TODO: create a setInterval function here to shuffle job titles, currently: Software Engineer & Web Master
+/**
+   * const jobTitles = [
+      'Software Engineer',
+      'Full-Stack Software Engineer',
+      'Cybersecurity Engineer',
+      'Webmaster',
+    ];
+    const circularJobTitleArray = jobTitles.concat([...jobTitles]);
+    let setSubHeadingChange;
+    // convert jobTitles to a circular array and loop over it
+    for (let i = 0; i < circularJobTitleArray.length; i++) {
+      setSubHeadingChange = circularJobTitleArray[i];
+    }
+    return setSubHeadingChange;
+   * 
+   * 
+   * 
+   */
