743062d223c610e982da03c7c2faf0fdfdb58c24
Dedios <dediosprogramming@gmail.com>
2024-06-23 22:14:20 -0400

    Footer refactor

--- portfolio1/src/components/Footer.tsx
+++ portfolio1/src/components/Footer.tsx
@@ -4,19 +4,18 @@ import { UilGithub } from '@iconscout/react-unicons';
 import { UilLinkedin } from '@iconscout/react-unicons';
 import { UilEnvelopeMinus } from '@iconscout/react-unicons';
 
-export default function Footer() {
+export default function Footer({ src }: { src: string }) {
   return (
     <>
       <div className="footer footer-center p-10 bg-creme text-navy">
-        <div>
+        <div className="mb-3">
           <Image src={logo} alt="Watermelon Me" width={250} height={100} />
-
           <p className="font-sans">
             Copyright © 2022-2024 - Created w/ ❤️ Eddie Padilla
           </p>
         </div>
         <div>
-          <div className="grid grid-flow-col gap-0.5">
+          <div className="grid grid-flow-col gap-1">
             <a href="https://www.linkedin.com/in/epadilla1/">
               <UilLinkedin />
             </a>
