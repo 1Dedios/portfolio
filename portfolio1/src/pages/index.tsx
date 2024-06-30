e183c4ad609e5e8b95f85f0dc984c557d2fc5cef
eddie-p <dediosprogramming@gmail.com>
2024-06-30 19:19:51 -0400

    removed Benefits component from homepage

--- portfolio1/src/pages/index.tsx
+++ portfolio1/src/pages/index.tsx
@@ -12,7 +12,6 @@ export default function Home() {
         <Navbar />
         <HeroSection />
         <ProjectSection />
-        <Benefits />
         <Contact />
         <Footer />
       </div>
