59a5340f34a0288f5db5ab7f4b3f7d434351999b
Eddie P <eddiep@eddies-mbp.mynetworksettings.com>
2024-06-29 23:19:41 -0400

    added typed animation to hero section

--- portfolio1/src/components/HeroSection.tsx
+++ portfolio1/src/components/HeroSection.tsx
@@ -1,10 +1,33 @@
 import Image from 'next/image';
+import Typed from 'typed.js'
+import { useEffect, useRef } from 'react';
 import { AttentionSeeker } from 'react-awesome-reveal';
 import watermelon from '/public/watermelon_me_bitmoji.png';
 
-// TODO: import text shuffle component to the sub-header under your name
 
 export default function HeroSection() {
+
+  const careerTitle = useRef(null);
+
+  useEffect(() => {
+    const titleType = new Typed(careerTitle.current, {
+      strings: [
+        'Software Engineer',
+        'Web Developer',
+        'Web Master',
+        'Security Engineer',
+        'Front-End Engineer',
+        'Full-Stack Developer',
+        'Back-End Engineer',
+        'Cybersecurity Engineer',
+      ],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true,
+      showCursor: false,
+    });
+    return () => titleType.destroy()});
+
   return (
     <div>
       <div
@@ -24,9 +47,8 @@ export default function HeroSection() {
               <AttentionSeeker effect="tada">Eddie P</AttentionSeeker>
             </h1>
             <h2
-              className={`font-poppins font-extrabold text-2xl italic mt-20 pt-5 text-creme`}
+              className={`font-poppins font-extrabold text-2xl italic mt-20 pt-5 text-creme`} ref={careerTitle}
             >
-              Software Engineer & Web Master
             </h2>
           </div>
           <span className="chat chat-end text-center w-64 h-40 inline-block absolute top-40 bottom-15 left-20 mx-80 my-40">
