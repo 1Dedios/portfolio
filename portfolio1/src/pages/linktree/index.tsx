5e89f0bdee674537ccd60062cbf71edd066e46bb
Dedios <dediosprogramming@gmail.com>
2024-08-17 22:34:29 -0400

    refactoring LinkTree route

--- portfolio1/src/pages/linktree/index.tsx
+++ portfolio1/src/pages/linktree/index.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image';
 import logo from '../../../public/dedios_logo.png';
+import Button from '../../components/Button';
+import {
+  UilGithubAlt,
+  UilLinkedinAlt,
+  UilDiscord,
+  UilLocationPinAlt,
+} from '@iconscout/react-unicons';
 
 export default function LinkTree() {
   // current link tree - https://linktr.ee/Dedios
   // TODO: bg ideally something animated
   // TODO: full stack software engineer - add typed so it can change this behavior
-  // TODO: social icons - linkedin, github, discord
   return (
     <>
       <div>
@@ -14,14 +20,22 @@ export default function LinkTree() {
           <Image src={logo} alt="logo" height={104} width={104} />
           <h1>Dedios</h1>
           <p>bible verse</p>
+          <div>
+            {/**LOCATION DIV */}
+            <UilLocationPinAlt />
+            <p>East Coast</p>
+          </div>
           <h1>Full Stack Software Engineer</h1>
-          <p>East Coast Engineer</p>
         </div>
         <div>
           <h1>div for social icons</h1>
+          <UilGithubAlt />
+          <UilLinkedinAlt />
+          <UilDiscord />
         </div>
         <div>
           <h1>div containing links</h1>
+          <Button text="a button" className="text-2xl text-center" />
         </div>
       </div>
     </>
