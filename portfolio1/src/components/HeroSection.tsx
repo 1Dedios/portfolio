d17b17d2207d2f1296d9f5285c7ffb43a6f1e80c
Dedios <dediosprogramming@gmail.com>
2025-03-27 23:10:18 -0400

    REFACTOR: Hero component

--- portfolio1/src/components/HeroSection.tsx
+++ portfolio1/src/components/HeroSection.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image';
-import Typed from 'typed.js';
-import { useEffect, useRef } from 'react';
-import { AttentionSeeker } from 'react-awesome-reveal';
-import watermelon from '/public/watermelon_me_bitmoji.png';
+import Image from "next/image";
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
+import { AttentionSeeker } from "react-awesome-reveal";
+import watermelon from "/public/watermelon_me_bitmoji.png";
 
 export default function HeroSection() {
   const careerTitle = useRef(null);
@@ -10,14 +10,15 @@ export default function HeroSection() {
   useEffect(() => {
     const titleType = new Typed(careerTitle.current, {
       strings: [
-        'Software Engineer',
-        'Web Developer',
-        'Web Master',
-        'Security Engineer',
-        'Front-End Engineer',
-        'Full-Stack Developer',
-        'Back-End Engineer',
-        'Cybersecurity Engineer',
+        "Software Engineer",
+        "Web Developer",
+        "Web Master",
+        "Security Engineer",
+        "Front-End Engineer",
+        "Full-Stack Developer",
+        "Back-End Engineer",
+        "Off-Sec Engineer",
+        "Cloud Engineer",
       ],
       typeSpeed: 100,
       backSpeed: 100,
@@ -25,38 +26,32 @@ export default function HeroSection() {
       showCursor: false,
     });
     return () => titleType.destroy();
-  });
+  }, []);
 
   return (
     <div>
       <div
         className="hero min-h-screen bg-auto bg-purple"
         style={{
-          backgroundImage: 'url(/dedios_logo.png)',
+          backgroundImage: "url(/dedios_logo.png)",
         }}
       >
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <div className="z-0 my-5">
-              <Image src={watermelon} alt="watermelon logo" />
-            </div>
-            <h1
-              className={`font-nosifer text-9xl text-center font-extrabold absolute -mx-20 -my-20 -z-10 text-creme`}
-            >
+            <Image src={watermelon} alt="watermelon-logo" className="my-5" />
+            <span className="rounded-lg p-4 bg-creme shadow-md shadow-slate-200 font-sans text-navy text-center w-64 absolute top-1/3 xl:left-40 lg:mx-40 lg:my-20 md:inline md:-left-0 md:mx-10">
+              Highly skilled, professional, polyglot Software Developer. I am passionate
+              about solving problems through Technology and building cool things 😛 for
+              the Human Race.
+            </span>
+            <div className="font-nosifer text-9xl text-center font-extrabold absolute -mx-20 -my-20 -z-10 text-creme">
               <AttentionSeeker effect="tada">Eddie P</AttentionSeeker>
-            </h1>
-            <h2
+            </div>
+            <div
               className={`font-poppins font-extrabold text-4xl italic mt-20 pt-5 text-creme`}
               ref={careerTitle}
-            ></h2>
+            ></div>
           </div>
-          <span className="chat text-center w-64 h-40 absolute top-40 bottom-15 left-20 mx-80 my-40 hidden md:-left-0 md:mx-10 lg:mx-60 lg:my-20 lg:block md:block">
-            <div className="chat-bubble bg-creme shadow-md shadow-slate-200 font-sans text-navy">
-              Highly skilled, professional, polyglot Software Developer. I am
-              passionate about solving problems through Technology and building
-              cool things 😛 for the Human Race.
-            </div>
-          </span>
         </div>
       </div>
     </div>
