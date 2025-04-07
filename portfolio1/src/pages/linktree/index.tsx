c107d0a0713fc5e663ee796c162c9b7b88d44b46
Dedios <dediosprogramming@gmail.com>
2025-04-07 15:54:54 -0400

    Feat: /linktree page finalized

--- portfolio1/src/pages/linktree/index.tsx
+++ portfolio1/src/pages/linktree/index.tsx
@@ -1,54 +1,60 @@
 import Image from "next/image";
+import Link from "next/link";
 import logo from "../../../public/watermelon_me_bitmoji.png";
 import Button from "../../components/Button";
-import {
-  UilGithubAlt,
-  UilLinkedinAlt,
-  UilDiscord,
-  UilLocationPinAlt,
-} from "@iconscout/react-unicons";
+import { UilGithubAlt, UilLinkedinAlt, UilDiscord } from "@iconscout/react-unicons";
 
 export default function LinkTree() {
-  // current link tree - https://linktr.ee/Dedios
   return (
     <>
-      <main>
-        {/**parent div for the whole page */}
-        <div className="flex flex-col items-center h-screen text-center">
-          <div>
-            <div className="flex flex-col items-center avatar mt-20 mb-6">
-              <div className="ring-creme ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
-                <Image src={logo} alt="logo" height={104} width={104} />
-              </div>
-              {/* TODO: add blinking cursor at the end of name */}
-              <h1 className="mt-4">
-                <span className="text-lime-400">:~$ </span>echo Dedios
-              </h1>
-              <p className="italic text-sm">ECC 11:2</p>
-            </div>
-            <h1>Tinkerer of Technology</h1>
-            <div className="flex flex-row justify-center space-x-1">
-              <UilLocationPinAlt />
-              <p>echo $HOME</p>
+      <div className="flex flex-col items-center h-screen text-center">
+        <div>
+          <div className="flex flex-col items-center avatar mt-20 mb-6">
+            <div className="ring-creme ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
+              <Image src={logo} alt="logo" height={104} width={104} />
             </div>
           </div>
-          <div className="flex flex-col space-y-4 mt-6 text-sm">
-            <Button
-              text="Dev Hardware Setup"
-              className="text-center glass rounded-lg pt-4 pb-4 pr-10 pl-10"
-            />
+          <p className="mt-4 text-creme">
+            <span className="text-lime-400">Dedios@linux-desktop:</span>~$ _
+          </p>
+          <p className="italic text-sm text-creme">+ ECC 11:2</p>
+        </div>
+        <div className="mt-4 w-7/12">
+          <p className="text-creme">
+            "Modern physics teaches me that nature is not capable of ordering itself. The
+            universe presupposes a huge mass of order. It therefore requires a great
+            "First Cause" that is not subject to the second law of transformation of
+            energy and that is therefore{" "}
+            <span className="text-creme underline decoration-4 decoration-gold italic">
+              Supernatural
+            </span>
+            ."
+          </p>
+          <br></br>
+          <p className="italic">- H.Aiken</p>
+        </div>
+        <div className="flex flex-col space-y-4 mt-6 text-sm">
+          <Link href="/" target="_blank">
             <Button
               text="My Site"
+              textClassName="text-lime-400 italic"
               className="text-center glass rounded-lg pt-4 pb-4 pr-10 pl-10"
             />
-          </div>
-          <div className="flex flex-row justify-center space-x-4 mt-6">
-            <UilGithubAlt />
-            <UilLinkedinAlt />
-            <UilDiscord />
-          </div>
+          </Link>
+          <Link href="/contact" target="_blank">
+            <Button
+              text="Contact"
+              textClassName="text-lime-400 italic"
+              className="text-center glass rounded-lg pt-4 pb-4 pr-10 pl-10"
+            />
+          </Link>
         </div>
-      </main>
+        <div className="flex flex-row justify-center space-x-4 mt-6">
+          <UilGithubAlt />
+          <UilLinkedinAlt />
+          <UilDiscord />
+        </div>
+      </div>
     </>
   );
 }
