15e3a771a051d44acda36baa8823ad443ac5d642
Dedios <dediosprogramming@gmail.com>
2025-04-18 22:48:44 -0400

    Feat: 404 pages implemented

---
+++ portfolio1/src/pages/sec-projects/index.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Footer from "@/components/Footer";
+
+export default function SecPage() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col items-center">
+        <Image src={"/images/404-nothere.png"} alt="404-icon" width={600} height={600} />
+      </div>
+      <Footer />
+    </>
+  );
+}
