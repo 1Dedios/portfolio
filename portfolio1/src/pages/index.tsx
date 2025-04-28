eb0f83f030ccfa80c9c36c9c561a779ed311e1fe
Dedios <dediosprogramming@gmail.com>
2025-04-28 17:55:53 -0400

    Refactored: FlipCard all buttons optional

--- portfolio1/src/pages/index.tsx
+++ portfolio1/src/pages/index.tsx
@@ -1,19 +1,17 @@
-import Navbar from '@/components/Navbar';
-import HeroSection from '@/components/HeroSection';
-import ProjectSection from '@/components/ProjectSection';
-import Footer from '@/components/Footer';
-import Contact from '@/components/Contact';
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import ProjectSection from "@/components/ProjectSection";
+import Footer from "@/components/Footer";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <Navbar />
-        <HeroSection />
-        <ProjectSection />
-        <Contact />
-        <Footer />
-      </div>
-    </main>
+    <div>
+      <Navbar />
+      <HeroSection />
+      <ProjectSection />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
--- portfolio1/src/pages/software-projects/index.tsx
+++ portfolio1/src/pages/software-projects/index.tsx
@@ -1,13 +1,57 @@
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
+import Button from "@/components/Button";
 import Footer from "@/components/Footer";
+import Image from "next/image";
 
 export default function SoftwarePage() {
+  // TODO: onClick handlers for each button to query for projects by lang
   return (
     <>
       <Navbar />
-      <div className="flex flex-col items-center">
-        <Image src={"/images/404-nothere.png"} alt="404-icon" width={600} height={600} />
+      <div className="bg-creme">
+        <p className="text-navy font-nosifer text-6xl text-center pt-4 pb-10">
+          Software Projects
+        </p>
+        <div className="flex flex-row justify-center space-x-4 pb-10">
+          <Button
+            text="JavaScript"
+            className="mt-2 p-1 bg-gold text-navy border-navy border-2 rounded-md"
+          ></Button>
+          <Button
+            text="C/C++/C#"
+            className="mt-2 p-1 bg-gold text-navy border-navy border-2 rounded-md"
+          ></Button>
+          <Button
+            text="Java"
+            className="mt-2 p-1 bg-gold text-navy border-navy border-2 rounded-md"
+          ></Button>
+          <Button
+            text="Python"
+            className="mt-2 p-1 bg-gold text-navy border-navy border-2 rounded-md"
+          ></Button>
+          {/**TODO: 2 options: 1-create component to make below reusable (ButtonAndImage - component) 2-keep buttons as is you'd only be adding an image to 2 buttons*/}
+          <button className="text-navy flex flex-row space-x-2 p-4 border-2 rounded-md">
+            Javascript
+            <span className="flex flex-row pl-2">
+              <Image
+                src="/images/tech-stack-logos/javascript.svg"
+                alt="src-alt"
+                width={16}
+                height={16}
+              ></Image>
+            </span>
+          </button>
+
+          <Button
+            text="AI/ML"
+            className="mt-2 p-1 bg-gold text-navy border-navy border-2 rounded-md"
+          ></Button>
+          <Button
+            text="Shell/Databases"
+            className="mt-2 p-1 bg-gold text-navy border-navy border-2 rounded-md"
+          ></Button>
+        </div>
+        {/* TODO: div for project query results to appear */}
       </div>
       <Footer />
     </>
